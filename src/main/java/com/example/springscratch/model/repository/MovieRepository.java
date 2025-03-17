582bb2dcc6421597a3ea9b27714f15d8455da3db
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-03-17 14:46:04 +0900

    리팩토링

--- src/main/java/com/example/springscratch/model/repository/MovieRepository.java
+++ src/main/java/com/example/springscratch/model/repository/MovieRepository.java
@@ -3,35 +3,43 @@ package com.example.springscratch.model.repository;
 import com.example.springscratch.model.dto.MovieDTO;
 import com.example.springscratch.model.dto.MovieParam;
 import com.example.springscratch.model.dto.MovieResponse;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.stereotype.Repository;
 
-import java.io.IOException;
 import java.net.URI;
-import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
 
 @Repository
 public class MovieRepository implements APIClientRepository {
-    final String url = "https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json";
+    final String baseURL = "https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice";
+    final String key = dotenv.get("MOVIE_KEY");
 
-    public List<MovieDTO> getMovies(MovieParam param) throws Exception {
-        final String key = dotenv.get("MOVIE_KEY");
+    public String callAPI(MovieParam param, String url, String action, String format) throws Exception {
+//        String action = "searchDailyBoxOfficeList";
+//        String format = "json";
+//        String url = "%s/%s.%s?key=%s&targetDt=%s"
+//                .formatted(baseURL, action, format, key, param.targetDate());
         HttpRequest request = HttpRequest.newBuilder()
                 // targetDt : yyyymmdd
-                .uri(URI.create("%s?key=%s&targetDt=%s".formatted(url, key, param.targetDate())))
+                .uri(URI.create(url.formatted(
+                        baseURL, action, format, key, param.targetDate())))
                 .GET()
                 .build();
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
         if (response.statusCode() == 200) {
-            MovieResponse movieResponse = objectMapper.readValue(response.body(), MovieResponse.class);
-            return movieResponse.boxOfficeResult().dailyBoxOfficeList()
-                    .stream().map((v) -> new MovieDTO(Long.parseLong(v.rank()), v.movieCd(), v.movieNm(), v.openDt(), Long.parseLong(v.audiAcc()))).toList();
-//            return objectMapper.readValue(response.body(), MovieDTO.class);
-//            System.out.println(response.body());
+            return response.body();
         }
         throw new RuntimeException("Failed : HTTP error code : " + response.statusCode());
+    }
+
+    public List<MovieDTO> getMovies(MovieParam param) throws Exception {
+        String action = "searchDailyBoxOfficeList";
+        String format = "json";
+        String url = "%s/%s.%s?key=%s&targetDt=%s";
+        String responseBody = callAPI(param, url, action, format);
+        MovieResponse movieResponse = objectMapper.readValue(responseBody, MovieResponse.class);
+        return movieResponse.boxOfficeResult().dailyBoxOfficeList()
+                .stream().map((v) -> new MovieDTO(Long.parseLong(v.rank()), v.movieCd(), v.movieNm(), v.openDt(), Long.parseLong(v.audiAcc()))).toList();
     }
 }
